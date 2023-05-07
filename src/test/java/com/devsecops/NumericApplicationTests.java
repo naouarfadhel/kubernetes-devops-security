20da311ff137b1ec2015112aac8c2fbf32eec7c7
Mohamed Fadhel NAOUAR <mfnaouar@vermeg.com>
2023-05-07 19:20:26 +0100

    changes

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -46,8 +46,6 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Greater than 50"));
     }
     
-
-    
 /*     @Test
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
@@ -57,4 +55,6 @@ public class NumericApplicationTests {
 public void welcomeMessage() throws Exception {
      this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("K8S DEVSECOPS"));
 }
+    
+
 }
