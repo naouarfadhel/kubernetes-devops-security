b7d69f7d8fa8ca1e455259d220640e7433471b09
Mohamed Fadhel NAOUAR <mfnaouar@vermeg.com>
2023-05-17 17:15:36 +0100

    fix* WebSecurityConfig.java

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsecops
+package com.devsecops;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
