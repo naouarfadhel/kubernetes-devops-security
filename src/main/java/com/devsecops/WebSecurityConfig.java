99355e08f4eafcc6541312fa3c8331f0e8495f12
Mohamed Fadhel NAOUAR <mfnaouar@vermeg.com>
2023-05-17 18:54:04 +0100

    added WebSecurityConfig.java*

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.devsecops;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.sun.xml.ws.developer.JAXWSProperties;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,6 +11,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         @SuppressWarnings("squid:S4430")
+
         http.csrf().disable();
     }
 }
