bc772b028299d7e81b235689afef4126f09d850c
Mohamed Fadhel NAOUAR <mfnaouar@vermeg.com>
2023-05-17 18:18:04 +0100

    added WebSecurityConfig.java

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -5,12 +5,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-
+        http
+            .csrf().disable();
     }
 }
