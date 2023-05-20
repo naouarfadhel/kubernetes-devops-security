a61d6a82fec907a17e6ee65f48fc477ef680ea50
Mohamed Fadhel NAOUAR <mfnaouar@vermeg.com>
2023-05-20 16:58:17 +0100

    refix role

--- zap.sh
+++ zap.sh
@@ -3,7 +3,7 @@
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 # first run this
-chmod 777 $(pwd)
+chmod 755 $(pwd)
 echo $(id -u):$(id -g)
 #docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
